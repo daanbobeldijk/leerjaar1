8d9608066f5d43a87397edcbd7fe8f6aa8475f96
daanbobeldijk <43110893+daanbobeldijk@users.noreply.github.com>
2018-09-12 12:46:04 +0200

    Add files via upload
    Changed the way of sequence input, also automated to ask if you want to calculate another sequence.

--- weektaak2/calculator.py
+++ weektaak2/calculator.py
@@ -1,16 +1,15 @@
-print("Filepath example:  /Users/daan/Downloads/School/Bio-Informatica/Lig1-DNA-Homo Sapiens.fasta.txt")
-print("Make sure there are no other 'G', 'C', 'T' or 'A' characters in the file!")
-var = input('Your filepath: ')
+var = input('Your sequence: ')
 if var == "" :
     var = input('Please enter a valid sequence: ')
 else :
-    bestand = open(var)
-    seq = bestand.read()
-    length = seq.count("G") + seq.count("C") + seq.count("T") + seq.count("A")
-    gc = seq.count("G") + seq.count("C")
-    check = round(gc/length * 100)
-    seqL = round(length / 4)
+    var = var.replace("\n", " ").replace("\r", " ").replace(" ", "")
+    # var = var.replace(' ', '')
+    length = var.count("G") + var.count("C") + var.count("T") + var.count("A")
+    gc = var.count("G") + var.count("C")
+    check = round(gc/len(var) * 100)
+    seqL = round(len(var) / 4)
     lengthStr = str(length) + ""
     nucL = str(seqL) + ""
-    print("\n", "This sequence has ", lengthStr, " nucleotides.")
+    print("\n", "This sequence has ", lengthStr, " characters.")
+    #print(" This sequence has ", nucL, " nucleotides.")
     print(" The GC percentage is: ", check, "%")
